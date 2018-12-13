b6c0512771a5a8a57ef1c3940466e68455c470da
Julien Valeriev Andonov <y9ann5@cz.ibm.com>
2018-12-13 10:57:25 +0100

    Second Commit

---
+++ Session 31 - Basic Crud Exercises/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeisterMask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TeisterMask.Data
+{
+    public class TeisterMaskDbContext : DbContext
+    {
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-4SV7N2B\\SQLEXPRESS;Database=TeisterMaskDb;Integrated Security = true");
+        }
+    }
+}

898852d8a94e31b4e4a4efd6b5440f72a6e5f819
Julien Valeriev Andonov <y9ann5@cz.ibm.com>
2018-12-16 08:45:19 +0200

    15122018

---
+++ Old Exams/PokemonEvolution/ChoreWarsV2/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace ChoreWarsV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int dishesTotalTime = 0;
+            int cleaningTotalTime = 0;
+            int laundryTotalTime = 0;
+
+
+            while (input != "wife is happy")
+            {
+                if (input.Contains("<") && input.Contains(">"))
+                {
+                    int startIndex = input.LastIndexOf("<");
+                    int endIndex = input.IndexOf(">");
+                    bool isValid = false;
+
+                    if (startIndex > endIndex)
+                    {
+                        for (int i = 0; i < endIndex; i++)
+                        {
+                            if (input[i] == '<')
+                            {
+                                startIndex = i;
+                            }
+                        }
+                    }
+
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        if (char.IsLower(input[i]) || char.IsDigit(input[i]))
+                        {
+                            isValid = true;
+                        }
+                        else
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        for (int i = startIndex + 1; i < endIndex; i++)
+                        {
+                            if (char.IsDigit(input[i]))
+                            {
+                                int time = int.Parse(input[i].ToString());
+                                dishesTotalTime += time;
+                            }
+
+                        }
+                    }
+                }
+                else if (input.Contains("[") && input.Contains("]"))
+                {
+                    int startIndex = input.LastIndexOf("[");
+                    int endIndex = input.IndexOf("]");
+                    bool isValid = false;
+                    
+                    if (startIndex > endIndex)
+                    {
+                        for (int i = 0; i < endIndex; i++)
+                        {
+                            if (input[i] == '[')
+                            {
+                                startIndex = i;
+                            }
+                        }
+                    }
+
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        if (char.IsUpper(input[i]) || char.IsDigit(input[i]))
+                        {
+                            isValid = true;
+                        }
+                        else
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        for (int i = startIndex + 1; i < endIndex; i++)
+                        {
+                            if (char.IsDigit(input[i]))
+                            {
+                                int time = int.Parse(input[i].ToString());
+                                cleaningTotalTime += time;
+                            }
+
+                        }
+                    }
+                }
+                else if (input.Contains("{") && input.Contains("}"))
+                {
+                    int startIndex = input.LastIndexOf("{");
+                    int endIndex = input.IndexOf("}");
+
+
+                    if (startIndex > endIndex)
+                    {
+                        for (int i = 0; i < endIndex; i++)
+                        {
+                            if (input[i] == '{')
+                            {
+                                startIndex = i;
+                            }
+                        }
+                    }
+
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        if (char.IsDigit(input[i]))
+                        {
+                            int time = int.Parse(input[i].ToString());
+                            laundryTotalTime += time;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int total = dishesTotalTime + cleaningTotalTime + laundryTotalTime;
+
+            Console.WriteLine($"Doing the dishes - {dishesTotalTime} min.");
+            Console.WriteLine($"Cleaning the house - {cleaningTotalTime} min.");
+            Console.WriteLine($"Doing the laundry - {laundryTotalTime} min.");
+            Console.WriteLine($"Total - {total} min.");
+
+        }
+    }
+}
---
+++ Old Exams/PokemonEvolution/ChoreWarsV3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ChoreWarsV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int dishesTotalTime = 0;
+
+            while (true)
+            {
+                if (input == "wife is happy")
+                {
+                    break;
+                }
+
+                if (input.Contains("<") && input.Contains(">"))
+                {
+                    int startIndex = input.IndexOf("<");
+                    int currentTime = 0;
+                    bool isValid = false;
+                    int endIndex = input.IndexOf(">");
+
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        if (input[i] == '<')
+                        {
+
+                            startIndex = i;
+                        }
+                    }
+
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        if (char.IsDigit(input[i]) || char.IsLower(input[i]))
+                        {
+                            isValid = true;
+                        }
+                        else
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                    
+                    for (int i = startIndex + 1; i < endIndex; i++)
+                    {
+                        if (isValid)
+                        {
+                            if (char.IsDigit(input[i]))
+                            {
+                                string digit = input[i].ToString();
+                                currentTime += int.Parse(digit);
+                            }
+                        }
+                    }
+
+                    dishesTotalTime += currentTime;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(dishesTotalTime);
+        }
+    }
+}

e286680377ac5e448f733e889819df415f98ec51
Julien Valeriev Andonov <y9ann5@cz.ibm.com>
2018-12-17 10:46:58 +0200

    17122018

---
+++ Old Exams/PokemonEvolution/BakingFactory/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BakingFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string bestBread = "";
+            int maxQuality = int.MinValue;
+            int minCount = int.MinValue;
+            double maxAvarage = double.MinValue;
+
+            while (input != "Bake It!")
+            {
+                List<int> breadQuality = input.Split("#", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+                int quality = breadQuality.Sum();
+                int count = breadQuality.Count();
+                double avarage = quality / count;
+
+                if (quality > maxQuality)
+                {
+                    maxQuality = quality;
+                    bestBread = String.Join(" ", breadQuality);
+                    minCount = count;
+                    maxAvarage = avarage;
+                }
+                else if (quality == maxQuality && avarage > maxAvarage)
+                {
+                    maxQuality = quality;
+                    bestBread = String.Join(" ", breadQuality);
+                    minCount = count;
+                    maxAvarage = avarage;
+                }
+                else if (quality == maxQuality && avarage == maxAvarage && count < minCount)
+                {
+                    maxQuality = quality;
+                    bestBread = String.Join(" ", breadQuality);
+                    minCount = count;
+                }
+                quality = -1;
+                count = -1;
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Best Batch quality: {maxQuality}");
+            Console.WriteLine($"{bestBread}");
+
+        }
+    }
+}
